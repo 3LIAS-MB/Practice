c763e687a8f49f4511fa99da84cbced2f1a96154
EliDev <eliasss.mb@gmail.com>
2024-09-27 21:07:14 -0300

    pern

--- 2- MERN/src/app.js
+++ 2- MERN/src/app.js
@@ -1,1 +1,27 @@
- 
+import express from "express";
+import morgan from "morgan";
+
+import taskRoutes from "./routes/task.routes.js" 
+import authRoutes from './routes/auth.routes.js'
+
+const app = express();
+
+// Middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get("/", (req, res) => res.json({ message: "welcome to my API XDD" }));
+app.use('/api', taskRoutes)
+app.use('/api', authRoutes)
+
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    status: "error",
+    message: err.message,
+  });
+});
+export default app;
